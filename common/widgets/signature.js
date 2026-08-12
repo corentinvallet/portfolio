39b7340690008b9b7165c63bc03c25130fd93cd5
cva <contact@corentinvallet.fr>
2026-08-12 23:25:09 +0200

    common - signature

--- common/widgets/signature.js
+++ common/widgets/signature.js
@@ -72,10 +72,10 @@
   var wrapper = document.createElement('div');
   wrapper.className = 'cv-signature';
   wrapper.innerHTML = `
-    <img class="cv-logo" src="https://corentinvallet.fr/common/widgets/cv-logo-orange.png" alt="Corentin Vallet" width="18" height="18" />
+    <a href="https://corentinvallet.fr?utm_source=signature-widget" target="_blank" rel="noopener"><img class="cv-logo" src="https://corentinvallet.fr/common/widgets/cv-logo-orange.png" alt="Corentin Vallet" width="18" height="18" /></a>
     <span>
-      Site réalisé par <a href="https://corentinvallet.fr?utm_source=signature-widget" target="_blank" rel="noopener">Corentin Vallet</a>
-      <span class="cv-tagline">— Création de sites pour artisans &amp; indépendants</span> 
+      <a href="https://corentinvallet.fr?utm_source=signature-widget" target="_blank" rel="noopener">Site réalisé par Corentin Vallet</a>
+      <span class="cv-tagline">— Création de sites web</span> 
     </span>
   `;
 
