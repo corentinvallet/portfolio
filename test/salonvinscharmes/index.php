e9f1771157bc556febfd41e0f331af3a5b88c296
cva <contact@corentinvallet.fr>
2026-09-16 09:41:56 +0200

    salonvinscharmes - Boutons accueil

--- test/salonvinscharmes/index.php
+++ test/salonvinscharmes/index.php
@@ -315,12 +315,12 @@ $svcRegionCounts = svc_region_counts($c['exposantsPage']['list'] ?? [], $svcSlug
       <p class="lede"><?= ml($hero['lede'] ?? '') ?></p>
       <div class="hero-actions">
         <?php if (($hero['ctaMode'] ?? 'classic') === 'invitation'): ?>
+          <a href="faq-club-oenologie.php" class="btn btn-ghost"><?= e($hero['ctaVisiteLabel'] ?? 'Préparer ma visite') ?></a>
           <?php if (!empty($hero['invitationPdf'])): ?>
           <a href="<?= e($hero['invitationPdf']) ?>" class="btn btn-solid" target="_blank" rel="noopener">
             <?= e($hero['ctaInvitationLabel'] ?? "Télécharger l'invitation") ?>
           </a>
           <?php endif; ?>
-          <a href="faq-club-oenologie.php" class="btn btn-ghost"><?= e($hero['ctaVisiteLabel'] ?? 'Préparer ma visite') ?></a>
         <?php else: ?>
           <a href="exposants-club-oenologie.php" class="btn btn-solid"><?= e($hero['ctaPrimary'] ?? '') ?></a>
           <a href="#activites" class="btn btn-ghost"><?= e($hero['ctaSecondary'] ?? '') ?></a>
